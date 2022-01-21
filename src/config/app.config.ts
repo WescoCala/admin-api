97533a3260c17e1372eb358c01b2742e4e6fb7a1
segalaz <65607178+segalaz@users.noreply.github.com>
2022-01-21 15:37:50 -0300

    cambios conexion

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -5,25 +5,25 @@ dotenv.config()
 
 export const config = {
 	mongodb: {
-		server: process.env.MONGO_CNN,
-		db: process.env.MONGO_DB,
+		server: process.env.MONGO_CNN || 'mongodb://localhost:27017/',
+		db: process.env.MONGO_DB || 'adinet',
 		auth: {
 			user: '',
 			pass: '',
 		},
 	},
 	sqlsrv: {
-		server: String(process.env.SQL_SERVER),
-		db: String(process.env.SQL_DB),
+		server: process.env.SQL_SERVER || '192.168.10.164',
+		db: process.env.SQL_DB || 'ADINET',
 		auth: {
-			user: String(process.env.SQL_USER),
-			pass: String(process.env.SQL_PASS),
+			user: process.env.SQL_USER || 'admadinet',
+			pass: process.env.SQL_PASS || '123456Aa',
 		},
 	},
 	server: {
 		port: process.env.PORT || 8080,
 	},
-	jwt_pass: process.env.JWT_SECRET || 'pass',
+	jwt_pass: process.env.JWT_SECRET || 'b^YZ8FTLtYV5mWva',
 	date: new Date(moment().format('YYYY-MM-DDTHH:mm:ss.SSS[Z]')),
 }
 
